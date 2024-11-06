5401ac0fb154254535abfc1d093a825dff7582cb
muriloferreiravitor <mferreiravitor@gmail.com>
2024-11-06 08:35:59 -0300

    fix dim_products and create fact_orders

--- models/marts/dim_products.sql
+++ models/marts/dim_products.sql
@@ -10,11 +10,35 @@ with
     )
     , dim_products as (
         select
-            *
+            stg_products.product_id
+
+            , stg_products.product_name
+            , stg_products.unit_price
+            , stg_products.quantity_per_unit
+            , stg_products.units_in_stock
+            , stg_products.units_on_order
+            , stg_products.reorder_level
+            , stg_products.is_discontinued
+
+            , stg_categories.category_name
+            , stg_categories.description
+
+            , stg_suppliers.company_name
+            , stg_suppliers.contact_name
+            , stg_suppliers.contact_title
+            , stg_suppliers.country
+            , stg_suppliers.region
+            , stg_suppliers.city
+            , stg_suppliers.address
+            , stg_suppliers.postal_code
+            , stg_suppliers.phone
+            , stg_suppliers.fax
+            , stg_suppliers.homepage
+
         from stg_products
-        join stg_suppliers
+        left join stg_suppliers
             on stg_products.supplier_id = stg_suppliers.supplier_id
-        join stg_categories
+        left join stg_categories
             on stg_products.category_id = stg_categories.category_id 
     )
 select * from dim_products
---
+++ models/marts/fact_orders.sql
@@ -0,0 +1,72 @@
+with
+    dim_customers as (
+        select * from {{ref('dim_customers')}}
+    )
+    , dim_employees as (
+        select * from {{ref('dim_employees')}}
+    )
+    , dim_products as (
+        select * from {{ref('dim_products')}}
+    )
+    , dim_shippers as (
+        select * from {{ref('dim_shippers')}}
+    )
+    , stg_orders as (
+        select
+            stg_orders.*
+        from {{ref('stg_orders')}}
+        left join dim_customers
+            on stg_orders.customer_id = dim_customers.customer_id
+        left join dim_employees
+            on stg_orders.employee_id = dim_employees.employee_id
+        left join dim_shippers
+            on stg_orders.shipper_id = dim_shippers.shipper_id
+    )
+    , stg_order_details as (
+        select
+            stg_order_details.*
+        from {{ref('stg_order_details')}}
+        left join dim_products
+            on stg_order_details.product_id = dim_products.product_id
+    )
+    , fact_orders as (
+        select
+
+            -- /* Primary Key */
+            stg_orders.order_id
+
+            -- /* Foreign Key */
+            , stg_order_details.product_id
+            , stg_orders.customer_id
+            , stg_orders.employee_id
+            , stg_orders.shipper_id
+
+            -- /* Order info */
+            , stg_orders.order_date
+            , stg_orders.required_date
+            , stg_orders.shipped_date
+            , stg_orders.freight
+            , stg_orders.ship_name
+            , stg_orders.ship_country
+            , stg_orders.ship_region
+            , stg_orders.ship_city
+            , stg_orders.ship_address
+            , stg_orders.ship_postal_code
+
+            -- /* Order details */            
+            , stg_order_details.unit_price
+            , stg_order_details.quantity
+            , stg_order_details.discount
+
+            -- /* Order details metrics */
+            , ((stg_order_details.discount / stg_order_details.unit_price) * 100) as discount_percent
+            , (stg_order_details.unit_price * stg_order_details.quantity) as order_details_total_price
+            , (stg_order_details.discount * stg_order_details.quantity) as order_details_total_discount
+            , ((stg_order_details.unit_price - stg_order_details.discount)
+                * stg_order_details.quantity) as order_details_final_price
+
+        from stg_orders
+        left join stg_order_details
+            on stg_orders.order_id = stg_order_details.order_id
+    )
+select * from fact_orders
